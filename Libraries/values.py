02f5325eb08fa891332209a98118947534ad88b6
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2020-08-17 11:04:36 +0200

    test data and jpgs changed

--- Libraries/values.py
+++ Libraries/values.py
@@ -1,5 +1,6 @@
 class positive_tests:
-    company_name = ['Kimpton Hotels & Restaurants', 'W. L. Gore & Associates', 'Empik', 'Helios S.A', "Jan Kowalski221"]
+    company_name = ['Kimpton Hotels & Restaurants', 'W. L. Gore & Associates', 'Grupa Helion SA', 'Helios S.A',
+                    "Jan Kowalski221"]
     e_mail = ['anyemail23@wp.pl', 'example@example.pl', 'brazil@example.br', 'mail@asd.us', 'vietnam@mail.vn']
     name_surname = ['Any Name', 'Alex Alex', 'Finlay McGraw', 'Jarret Roberts', 'Makena Woods']
     phone_number = ['435654234', '000000000', '987312476', '543765321', '765432358']
@@ -7,7 +8,8 @@ class positive_tests:
 
 
 class negative_tests_password:
-    company_name = ['Kimpton Hotels & Restaurants', 'W. L. Gore & Associates', 'Empik', 'Helios S.A', "Jan Kowalski221"]
+    company_name = ['Kimpton Hotels & Restaurants', 'W. L. Gore & Associates', 'Grupa Helion SA', 'Helios S.A',
+                    "Jan Kowalski221"]
     e_mail = ['anyemail23@wp.pl', 'example@example.pl', 'brazil@example.br', 'mail@asd.us', 'vietnam@mail.vn']
     name_surname = ['Any Name', 'Alex Alex', 'Finlay McGraw', 'Jarret Roberts', 'Makena Woods']
     phone_number = ['435654234', '000000000', '987312476', '543765321', '765432358']
@@ -15,8 +17,10 @@ class negative_tests_password:
 
 
 class negative_tests_password_companyname:
-    company_name = ['Kimpton Hotels & Restaurants', 'W. L. Gore & Associates', 'Empik', 'Helios S.A', "Jan Kowalski221"]
+    company_name = ['Kimpton Hotels & Restaurants', 'W. L. Gore & Associates', 'Grupa Helion SA', 'Helios S.A',
+                    "Jan Kowalski221"]
     e_mail = ['anyemail23@wp.pl', 'example@example.pl', 'example@example.pl', 'mail@asd.pl', 'example@gmail.com']
     name_surname = ['Any Name', 'Alex Alex', 'Finlay McGraw', 'Jarret Roberts', 'Makena Woods']
     phone_number = ['435654234', '000000000', '987312476', '543765321', '765432358']
-    password = ['Kimpton Hotels & Restaurants', 'W. L. Gore & Associates', 'Empik', 'Helios S.A', "Jan Kowalski221"]
+    password = ['Kimpton Hotels & Restaurants', 'W. L. Gore & Associates', 'Grupa Helion SA', 'Helios S.A',
+                "Jan Kowalski221"]
